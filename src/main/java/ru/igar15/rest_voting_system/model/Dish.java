e68d7946156ea391a03a65386f139112695cc70d
igar15 <igar15@yandex.ru>
2021-02-28 20:11:44 +0300

    Make Dish JPA Entity

--- src/main/java/ru/igar15/rest_voting_system/model/Dish.java
+++ src/main/java/ru/igar15/rest_voting_system/model/Dish.java
@@ -1,8 +1,16 @@
 package ru.igar15.rest_voting_system.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dishes")
 public class Dish extends AbstractNamedEntity{
 
+    @Column(name = "price")
     private int price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
     private Menu menu;
 
     public Dish() {
