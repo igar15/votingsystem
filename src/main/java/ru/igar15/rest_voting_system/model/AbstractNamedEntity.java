ebf81a39e3a1b08b6e099d52138369e3bc434794
igar15 <igar15@yandex.ru>
2021-02-28 15:45:05 +0300

    Reformat code

--- src/main/java/ru/igar15/rest_voting_system/model/AbstractNamedEntity.java
+++ src/main/java/ru/igar15/rest_voting_system/model/AbstractNamedEntity.java
@@ -24,4 +24,9 @@ public abstract class AbstractNamedEntity extends AbstractBaseEntity {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return super.toString() + '(' + name + ')';
+    }
 }
