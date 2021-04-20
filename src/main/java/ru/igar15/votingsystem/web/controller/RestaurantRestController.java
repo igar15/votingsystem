0859f8c95961389aa9862f8528eeebfc481cf293
igar15 <igar15@yandex.ru>
2021-04-20 21:35:26 +0300

    Clear code

--- src/main/java/ru/igar15/votingsystem/web/controller/RestaurantRestController.java
+++ src/main/java/ru/igar15/votingsystem/web/controller/RestaurantRestController.java
@@ -3,7 +3,6 @@ package ru.igar15.votingsystem.web.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -27,9 +26,6 @@ public class RestaurantRestController {
 
     @Autowired
     private RestaurantService service;
-
-    @Autowired
-    private ApplicationContext context;
 
     @GetMapping
     public List<Restaurant> getAll() {
